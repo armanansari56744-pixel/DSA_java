0fb9001b80ff2fd26956ac7a5aeae80c75a007e2
armanansari56744-pixel <armanansari56744@gmail.com>
2026-08-16 14:00:21 +0530

    Double LinkedList insertion in beginning , end and specific location

--- Double_LinkedList_insertion.java
+++ Double_LinkedList_insertion.java
@@ -17,7 +17,7 @@ public class Double_LinkedList_insertion {
     Node tail = null;
 
     public void insert() {
-        int n;
+        int n, choice, pos;
         Scanner sc = new Scanner(System.in);
         do {
             System.out.println("Enter data: ");
@@ -29,9 +29,46 @@ public class Double_LinkedList_insertion {
                 head = new_node;
                 tail = new_node;
             } else {
-                head.prev = new_node;
-                new_node.next = head;
-                head = new_node;
+                System.out.println("=== Enter choice===");
+                System.out.println("1. At beginning: ");
+                System.out.println("2. At End: ");
+                System.out.println("3. At specific Location: ");
+
+                choice = sc.nextInt();
+
+                switch (choice) {
+                    case 1:
+                        new_node.next = head;
+                        head.prev = new_node;
+                        head = new_node;
+
+                        break;
+                    case 2:
+                        tail.next = new_node;
+                        new_node.prev = tail;
+                        tail = new_node;
+                        break;
+
+                    case 3:
+                        System.out.println("Enter position: ");
+                        pos = sc.nextInt();
+
+                        Node temp = head;
+                        Node temp1 = temp.next;
+                        for(int i =1 ;i<(pos-1); i++){
+                            temp = temp1;
+                            temp1= temp.next;
+                        }
+                        new_node.prev = temp;
+                        new_node.next= temp1;
+                        temp.next = new_node;
+                        temp1.prev= new_node;
+                        break;
+
+                    default:
+                        System.out.println("Invalid choice :");
+                        break;
+                }
             }
             System.out.println("Do you want to add more data press: 7 ");
             n = sc.nextInt();
@@ -60,62 +97,62 @@ public class Double_LinkedList_insertion {
     }
 
     // static class Node {
-    //     int data;
-    //     Node next;
-    //     Node prev;
+    // int data;
+    // Node next;
+    // Node prev;
 
-    //     Node(int data) {
-    //         this.data = data;
-    //         this.next = null;
-    //         this.prev = null;
-    //     }
+    // Node(int data) {
+    // this.data = data;
+    // this.next = null;
+    // this.prev = null;
+    // }
     // }
 
     // Node head = null;
     // Node tail = null;
 
     // public void insert(int data) {
-    //     //
+    // //
 
-    //     Node new_node = new Node(data);
-    //     if (head == null) {
+    // Node new_node = new Node(data);
+    // if (head == null) {
 
-    //         head = new_node;
-    //         tail = new_node;
-    //     } else {
-    //         head.prev = new_node;
-    //         new_node.next = head;
-    //         head = new_node;
-    //     }
+    // head = new_node;
+    // tail = new_node;
+    // } else {
+    // head.prev = new_node;
+    // new_node.next = head;
+    // head = new_node;
+    // }
     // }
 
     // public void display() {
-    //     Node temp = head;
+    // Node temp = head;
 
-    //     if (head == null) {
-    //         System.out.println("List is empty: ");
-    //     } else {
-    //         while (temp != null) {
-    //             System.out.print(temp.data + " <-> ");
-    //             temp = temp.next;
-    //         }
-    //         System.out.println("null");
-    //     }
+    // if (head == null) {
+    // System.out.println("List is empty: ");
+    // } else {
+    // while (temp != null) {
+    // System.out.print(temp.data + " <-> ");
+    // temp = temp.next;
+    // }
+    // System.out.println("null");
+    // }
     // }
 
     // public static void main(String[] args) {
-    //     Double_LinkedList_insertion dll = new Double_LinkedList_insertion();
+    // Double_LinkedList_insertion dll = new Double_LinkedList_insertion();
 
-    //     dll.insert(10);
-    //     dll.insert(20);
-    //     dll.insert(30);
-    //     dll.insert(40);
-    //     dll.insert(50);
-    //     dll.insert(60);
-    //     dll.insert(70);
-    //     dll.insert(80);
+    // dll.insert(10);
+    // dll.insert(20);
+    // dll.insert(30);
+    // dll.insert(40);
+    // dll.insert(50);
+    // dll.insert(60);
+    // dll.insert(70);
+    // dll.insert(80);
 
-    //     dll.display();
+    // dll.display();
     // }
 
 }
