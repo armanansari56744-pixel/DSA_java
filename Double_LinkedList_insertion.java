62e3792454e8e4269eb926e7dcd8c4cef4476117
armanansari56744-pixel <armanansari56744@gmail.com>
2026-08-16 14:03:43 +0530

    Update Double_LinkedList_insertion.java

--- Double_LinkedList_insertion.java
+++ Double_LinkedList_insertion.java
@@ -29,46 +29,9 @@ public class Double_LinkedList_insertion {
                 head = new_node;
                 tail = new_node;
             } else {
-                System.out.println("=== Enter choice===");
-                System.out.println("1. At beginning: ");
-                System.out.println("2. At End: ");
-                System.out.println("3. At specific Location: ");
-
-                choice = sc.nextInt();
-
-                switch (choice) {
-                    case 1:
-                        new_node.next = head;
-                        head.prev = new_node;
-                        head = new_node;
-
-                        break;
-                    case 2:
-                        tail.next = new_node;
-                        new_node.prev = tail;
-                        tail = new_node;
-                        break;
-
-                    case 3:
-                        System.out.println("Enter position: ");
-                        pos = sc.nextInt();
-
-                        Node temp = head;
-                        Node temp1 = temp.next;
-                        for(int i =1 ;i<(pos-1); i++){
-                            temp = temp1;
-                            temp1= temp.next;
-                        }
-                        new_node.prev = temp;
-                        new_node.next= temp1;
-                        temp.next = new_node;
-                        temp1.prev= new_node;
-                        break;
-
-                    default:
-                        System.out.println("Invalid choice :");
-                        break;
-                }
+               head.prev = new_node;
+               new_node.next = head;
+               head= new_node;
             }
             System.out.println("Do you want to add more data press: 7 ");
             n = sc.nextInt();
